43b4bbfd34910d447cab5b238db41bbd456c1e9c
rahaf89 <rahaflh.900@hotmail.com>
2020-05-24 22:46:09 +0200

    fixing the logo

--- src/Components/TieInBar.js
+++ src/Components/TieInBar.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import "./main.css";
 import { Link } from "react-router-dom";
 
+
 function TieInBar({ profileLink }) {
   return (
     <div className="container">
       <div className="sidebar tie-in base">
         <br />
         <img
-          src="https://lh3.googleusercontent.com/proxy/ufzV0ryl2xgcaB1VID7OTdSp91Ic2ROe0wvM-BUr_WaYLiQw739abu2BhwTxx90QbFwmXaR_jyDB4MJOJN5RBjVk16R8lh8aQLynAu2zBU4BnrfcVf3b7vAoPlhCxGjh4ArP2bkllFCzTXjjtdaiJjjy0lQhgpqCwFJdT5W5ZnDO9w"
+          src={process.env.PUBLIC_URL + '/logo.png'}
           width="35px"
           height="35px"
           className="logo"
